1a81dc74f5ddcecc7f3940d9f0935dd486d7077c
Hunter Dyar <bloop@hdyar.com>
2020-01-27 16:36:04 -0500

    Initial Commit

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public ContactFilter2D filter2D;
+    private BoxCollider2D boxCol;
+    public Vector2Int levelSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(levelSize == Vector2Int.zero){
+            Debug.LogError("Will cause infinite loop");
+        }
+        boxCol = GetComponent<BoxCollider2D>();
+        MoveToRandomSpot();
+    }
+    void MoveToRandomSpot(){
+        transform.position = GetRandomSpot();
+        Collider2D[] results = new Collider2D[1];
+        boxCol.OverlapCollider(filter2D,results);
+        if(results[0] != null){
+            Debug.Log("picked occupied spot, "+transform.position);
+            //MoveToRandomSpot();
+        }
+    }
+    Vector3 GetRandomSpot(){
+        int randomX = Random.Range(-(int)levelSize.x/2,(int)levelSize.x/2);
+        int randomY = Random.Range(-(int)levelSize.y/2,(int)levelSize.y/2);
+        return new Vector3((int)randomX,(int)randomY,0);
+    }
+}
---
+++ Assets/Scripts/SnakeMovement.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour
+{
+   
+    public ContactFilter2D filter2D;
+    public GameObject fruitPrefab;
+    Vector2 currentDirection;
+    BoxCollider2D boxCol;
+
+    //timer used to move ever x seconds
+    float timer;
+    public float moveInterval; //as this decreases, the game goes faster.
+
+    int turn;
+    void Awake(){
+        boxCol = GetComponent<BoxCollider2D>();
+    }
+    void Start()
+    {
+        turn = 0;
+        timer = 0;
+        currentDirection = Vector2.right;
+        //Start with a fruit.
+        DropNewFruit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {      
+        CheckInput();
+        timer = timer+Time.deltaTime;
+        if(timer >= moveInterval){
+            Move();
+            timer = 0;
+        }
+    }
+    void Move(){
+        transform.position = transform.position+new Vector3(currentDirection.x,currentDirection.y,0);
+        turn++;
+        if(turn%10 == 0){//if the remainder turn divided by 10 is zero. IE if turn is a multiple of 10.
+            moveInterval = moveInterval - moveInterval*0.1f;//10% faster!
+        }
+        if(CheckOverlap() != null){
+            Transform overlapping = CheckOverlap();
+            if(overlapping.tag == "Tail"){
+                Debug.Log("You Lose");
+                Death();
+            }else if(overlapping.tag == "Fruit"){
+                Debug.Log("New Tail Piece");
+                Destroy(overlapping.gameObject);
+                DropNewFruit();
+            }
+        }
+        
+    }
+    void CheckInput(){
+        if(Input.GetKeyDown(KeyCode.DownArrow) && currentDirection != Vector2.up){
+            currentDirection = Vector2.down;
+        }else if(Input.GetKeyDown(KeyCode.UpArrow) && currentDirection != Vector2.down){
+            currentDirection = Vector2.up;
+        }else if(Input.GetKeyDown(KeyCode.LeftArrow) && currentDirection != Vector2.right){
+            currentDirection = Vector2.left;
+        }else if(Input.GetKeyDown(KeyCode.RightArrow) && currentDirection != Vector2.left){
+            currentDirection = Vector2.right;
+        }
+    }
+    //Returns the transform of what we are overlapping.
+    Transform CheckOverlap(){
+        Collider2D[] results = new Collider2D[1];
+        boxCol.OverlapCollider(filter2D,results);
+        if(results[0] != null){
+            return results[0].transform;
+        }else{
+            return null;
+        }
+    }
+    //Creates a new fruit. Does not move it into position.
+    void DropNewFruit(){
+        GameObject.Instantiate(fruitPrefab,Vector3.zero,Quaternion.identity);
+    }
+    void Death(){
+        moveInterval = Mathf.Infinity;
+    }
+}
